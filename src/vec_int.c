3299c85a69c9f869c0740dfb59cec76f4cd26a6a
Rahul Rajeev Pillai <66192267+Ashborn-SM@users.noreply.github.com>
2022-12-30 21:19:12 +0530

    Update vec_int.c

--- src/vec_int.c
+++ src/vec_int.c
@@ -1,28 +1,46 @@
 #include "vec_int.h"
 #include <math.h>
-extern float* voltages;
-extern int counter;
+
+float voltages[BIN_SIZE][MAX_SIZE];
+int counter = 0;
+int bin = 0;
+
+__irq void TIMER_ISR(void){
+	T0IR ^= 0x1;
+	AD0CR |= (1 << 24) ;
+}
+__irq void UART_ISR(void){}
 
 __irq void ADC_ISR(void){
 	int result = (AD0DR1 >> 6) & 0x3FF;
-	voltages[counter++] = result/1023 * 3.3;
-	
-	if(counter == MAX_SIZE){
+	voltages[bin][counter++] = result/1023 * 3.3;
+
+	if(counter*bin == MAX_SIZE){
 		/*
 		 'n' - Hanning Window
 		 'm' - Hamming Window
 		 'b' - Blackman Window
 		 'r' - Rectangular Window
 		 */
-		CMPLX* freq_components = STFT(voltages, 'n');
+		CMPLX** freq_components = STFT(voltages, 'n');
 		
 		float magnitude[MAX_SIZE];
-		for(int i=0; i<MAX_SIZE; i++){
-			magnitude[i] = sqrt(pow(creal(freq_components[i]), 2) + pow(cimag(freq_components[i]), 2));
+		for(int i=0, k=0; i<BIN_SIZE; i++){
+			for(int j=0; j<BIN_SIZE; j++){
+				CMPLX f_c = freq_components[i][j];
+				magnitude[k++] = sqrt(pow(creal(f_c), 2) + pow(cimag(f_c), 2));
+			}
 		}
-		// UART
-		
-		//
+			
+
+		counter = 0;
+		bin = 0;
+	}
+	
+	if(counter == 16){ 
+		bin++; 
 		counter = 0;
 	}
+	
+	VICVectAddr = 0x00; // exiting the  
 }
