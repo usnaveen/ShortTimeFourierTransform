65ff50c55175159782f4753fe7e488ab145bd28b
Rahul Rajeev Pillai <66192267+Ashborn-SM@users.noreply.github.com>
2022-12-30 21:18:46 +0530

    Update stft.h

--- src/stft.h
+++ src/stft.h
@@ -4,9 +4,10 @@
 #include <complex.h>
 
 #define MAX_SIZE 128
+#define BIN_SIZE 16
 
 typedef float complex CMPLX;
 
-CMPLX* STFT(float voltages[MAX_SIZE], char window);
+CMPLX** STFT(float voltages[BIN_SIZE][MAX_SIZE], char window_char);
 
 #endif
