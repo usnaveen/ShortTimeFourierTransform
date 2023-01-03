490590a48cca42f48e32564a33a0e6bb859a450f
NAVEEN US <88378808+usnaveen@users.noreply.github.com>
2023-01-03 20:58:59 +0530

    Update UART.h

--- src/UART.h
+++ src/UART.h
@@ -2,15 +2,17 @@ void initUART(void);
 void sendUART(unsigned char mydata);
 unsigned char receiveUART(void);
 void printUART(unsigned char mydata[]);
+
 void initUART(void)
 {
 initPLL();
-PINSEL0 = 0x05;
+PINSEL0 = 0x05; // UART0 TXD - P0.0, RXD - P0.1
 U0LCR = 0x83; // DLAB=1 , 11 8bit UART
 U0DLL = 0x87; // 0x0187 for 9600 (391)
-U0DLM = 0x01;
+U0DLM = 0x01; // 
 U0LCR = 0x03; // DLAB=0 , 11 8bit UART
 }
+
 void sendUART(unsigned char mydata)
 {
 U0THR = mydata;
@@ -24,6 +26,7 @@ while ((U0LSR & 0x01)== 0);
 mydata = U0RBR;
 return mydata;
 }
+
 void printUART(unsigned char mydata[])
 {
 int i=0;
